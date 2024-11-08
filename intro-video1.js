528e2a501c9b35de86173ce9917f716d5fdc8d31
Lubna5Sh <eaa24las@students.eaaa.dk>
2024-11-08 12:06:28 +0000

    Ændringer i index,style

---
+++ intro-video1.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const video = document.querySelector('.video1');
+    const nextPageIcon = document.getElementById('nextPageIcon');
+
+    video.addEventListener('ended', function() {
+        nextPageIcon.style.display = 'block';
+    });
+
+    nextPageIcon.addEventListener('click', function() {
+        window.location.href = 'nextpage.html'; // Erstat 'nextpage.html' med din næste side URL
+    });
+});
